93f591af955c6cd15ff91cab9443aaadfd2a855d
Mpho <130040248+Mselemela@users.noreply.github.com>
2026-03-20 15:25:48 +0000

    newsfeed

---
+++ newsfeed/Newsfeed.java
@@ -0,0 +1,12 @@
+
+public class Newsfeed {
+  public Newsfeed(){
+      
+  }
+      
+  // Create getTopics() below:
+  public String[] getTopics(){
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    return topics;
+  }
+}

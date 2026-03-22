a1908a95b8a7d101ab26ebce67fa019a6ce88f9f
Mpho <130040248+Mselemela@users.noreply.github.com>
2026-03-22 04:29:07 +0000

    length

---
+++ newsfeed/newsfeed/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Newsfeed sampleFeed = new Newsfeed();
+        System.out.println("the number of topics is: " + sampleFeed.getNumTopics());
+    }
+}
---
+++ newsfeed/newsfeed/Newsfeed.java
@@ -0,0 +1,16 @@
+public class Newsfeed {
+    String[] topics = { "Opinion", "Tech", "Science", "Health" };
+    int[] views = { 0, 0, 0, 0 };
+
+    public Newsfeed() {
+
+    }
+
+    public String[] getTopics() {
+        return topics;
+    }
+
+    public int getNumTopics() {
+        return topics.length;
+    }
+}

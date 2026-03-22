a51df2426f7cd1f3d901b7c176e6b77db62d97cd
Mpho <130040248+Mselemela@users.noreply.github.com>
2026-03-22 02:05:49 +0000

    viewTopic

--- newsfeed/Newsfeed.java
+++ newsfeed/Newsfeed.java
@@ -1,9 +1,4 @@
-
 public class Newsfeed {
-  public Newsfeed(){
-      
-  }
-      public class Newsfeed {
   String[] topics = {"Opinion", "Tech", "Science", "Health"};
   public int[] views = {0, 0, 0, 0};
   
@@ -15,19 +10,13 @@ public class Newsfeed {
     return topics;
   }
   
-  // getFirstTopic() below
+  // Create getFirstTopic() below
   public String getFirstTopic() {
     return topics[0];
   }
 
   
   public void viewTopic(int topicIndex){
-    
-  }
-}
-  // getTopics() below:
-  public String[] getTopics(){
-    String[] topics = {"Opinion", "Tech", "Science", "Health"};
-    return topics;
+   views[topicIndex]++;
   }
 }

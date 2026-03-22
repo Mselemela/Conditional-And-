faf873c55d453fcf85a7c3e80054633b2df56e66
Mpho <130040248+Mselemela@users.noreply.github.com>
2026-03-22 01:59:47 +0000

    getTopics

--- newsfeed/Main.java
+++ newsfeed/Main.java
@@ -1,7 +1,10 @@
+// import the array package
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
     Newsfeed sampleFeed = new Newsfeed();
     String[] topics = sampleFeed.getTopics();
-    System.out.println(topics);
+    System.out.println(Arrays.toString(topics));
   }
 }
--- newsfeed/Newsfeed.java
+++ newsfeed/Newsfeed.java
@@ -3,8 +3,29 @@ public class Newsfeed {
   public Newsfeed(){
       
   }
-      
-  // Create getTopics() below:
+      public class Newsfeed {
+  String[] topics = {"Opinion", "Tech", "Science", "Health"};
+  public int[] views = {0, 0, 0, 0};
+  
+  public Newsfeed(){
+  
+  }
+    
+  public String[] getTopics(){
+    return topics;
+  }
+  
+  // getFirstTopic() below
+  public String getFirstTopic() {
+    return topics[0];
+  }
+
+  
+  public void viewTopic(int topicIndex){
+    
+  }
+}
+  // getTopics() below:
   public String[] getTopics(){
     String[] topics = {"Opinion", "Tech", "Science", "Health"};
     return topics;

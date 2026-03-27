a7df9c2db92047af88a5baf127401fe4ec7a61d7
Mpho <130040248+Mselemela@users.noreply.github.com>
2026-03-27 15:02:14 +0000

    todo1

---
+++ Arrays/todo1/ToDo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+    public static void main(String[ args) {
+
+        ArraList<String> toDoList = new ArrayList<String>(;
+            String toDo1 = "Water plants";
+
+            String toDo2 = "Take a walk";
+            String toDo3 = "Have breakfast";
+
+            toDoList.add(toDo1);
+            toDoList.add(toDo2);
+            toDoList.add(toDo3);
+
+            System.out.println(toDoList);
+        )
+    }])
+}
